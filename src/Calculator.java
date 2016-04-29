3cd506bd942de3245d2a9281a69a103a9303af4b
Karthik128 <dr.m.karthiik@gmail.com>
2016-04-29 23:03:03 +0530

    Calculator App

---
+++ src/Calculator.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+     MathMethods Math=new MathMethods();
+     
+     System.out.println("Maths Calculator App");
+     System.out.println("********************");
+     
+     Scanner userInput=new Scanner(System.in);
+     
+     System.out.println("Enter First Nuumber");
+     
+     Integer num1=userInput.nextInt();
+     
+     System.out.println("Enter Second Nuumber");
+     
+     Integer num2=userInput.nextInt();
+     
+     System.out.println("Please select the operation +,-,*,/");
+     
+     String operation = userInput.next();
+     
+     if (operation.equals("+")){
+    	  
+    	 System.out.println("Adding "+num1+" & "+num2+" gives");
+    	 System.out.println(Math.Add(num1, num2));
+     }
+     
+     else if (operation.equals("-")){
+    	 
+    	 System.out.println("Subsctraction of "+num1+" & "+num2+" gives");
+    	 System.out.println(Math.Substract(num1, num2));
+     }
+     
+     else if (operation.equals("*")){
+    	 
+    	 System.out.println("Multiplication of "+num1+" & "+num2+" gives");
+    	 System.out.println(Math.Multply(num1, num2));
+     }
+     
+     else if (operation.equals("/")){
+    	 
+    	 System.out.println("Division of"+num1+" & "+num2+" gives");
+    	 System.out.println(Math.Division(num1, num2));
+     }
+     
+     
+     
+     
+	}
+
+}
---
+++ src/MathMethods.java
@@ -0,0 +1,24 @@
+
+public class MathMethods {
+
+	public int Add(int num1,int num2 ){
+		
+		return num1+num2;
+	}
+	
+	public int Substract(int num1, int num2){
+		
+		return num1-num2;
+	}
+	
+	public int Multply(int num1, int num2){
+		
+		return num1*num2;
+	}
+	
+	public int Division(int num1, int num2){
+		
+		return num1/num2;
+	}
+
+}
